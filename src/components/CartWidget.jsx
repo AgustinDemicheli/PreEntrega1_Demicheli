518be7c1d5e927f53e5a712182a5d89bbbcec8dd
ademicheli <ademicheli@jusbaires.gob.ar>
2023-12-11 21:10:07 -0300

    initial commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+
+const CartWidget = () => {
+  return (
+    <div className="cart-widget">
+      <FaShoppingCart size={24} />
+      <span className="badge badge-primary">5</span>
+    </div>
+  );
+};
+
+export default CartWidget;
